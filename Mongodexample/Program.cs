b927590177fb251a69dce05a7e17943c1c5952d6
Yentl Bekaert <yentl.bekaert@student.vives.be>
2021-12-08 10:33:34 +0100

    update to allow Access-Control-Allow-Origin

--- Mongodexample/Program.cs
+++ Mongodexample/Program.cs
@@ -26,6 +26,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
